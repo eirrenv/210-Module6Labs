411aeae32d29d53ff41cbe354fb6dc5cd8b02106
Eirren <edv2019@gmail.com>
2026-03-03 18:29:43 -0800

    Initial commit, pasted Lab 18 code

---
+++ Lab19.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// review Node struct
+struct Node {
+    double rating;
+    string review;
+    Node *next = nullptr;
+};
+
+// entering review function prototype
+void enterReview(Node *&reviewNode);
+
+// insert at head of linked list function prototype
+void insertAtHead(Node *&headNode);
+
+// insert at tail of linked list prototype
+void insertAtTail(Node *&headNode);
+
+// print reviews prototype
+void printReviews(Node *headNode);
+
+int main() {
+    Node *head = nullptr;
+
+    int insertChoice; // int to contain LL method
+
+    cout << "Which linked list method should we use?" << endl;
+    cout << "\t[1] New nodes are added at the head of the linked list" << endl;
+    cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
+    cout << "\tChoice: ";
+    cin >> insertChoice;
+
+    char yn = 'y'; // Y/N check
+
+    if (insertChoice == 1) {
+        while (tolower(yn) != 'n') {
+            insertAtHead(head);
+            cout << "Enter another review? Y/N: ";
+            cin >> yn;
+        }
+        printReviews(head);
+    }
+    else if (insertChoice == 2) {
+        while (yn != 'n' && yn != 'N') {
+            insertAtTail(head);
+            cout << "Enter another review? Y/N: ";
+            cin >> yn;
+        }
+        printReviews(head);
+    }
+    return 0;
+}
+
+// enter reviews 
+void enterReview(Node *&reviewNode) {
+    cout << "Enter review rating 0-5: ";
+    cin >> reviewNode->rating;
+    cin.ignore();
+    cout << "Enter review comments: ";
+    getline(cin, reviewNode->review);
+}
+
+// print reviews and calculate and print average
+void printReviews(Node *headNode) {
+    cout << "Outputting all reviews:" << endl;
+    if (headNode == nullptr) {
+        cout << "\tNo reviews to output." << endl;
+    }
+    else {
+        Node *current = headNode;
+        int count = 0;
+        double total = 0.0;
+        while (current != nullptr) {
+            cout << "\t> Review #" << count + 1 << ": " << current->rating << ": " << current->review << endl;
+            total += current->rating;
+            ++count;
+            current = current->next;
+        }
+        cout << "\t> Average: " << total / count << endl;
+
+    }
+}
+
+// insert node at head
+void insertAtHead(Node *&headNode) {
+    Node *newNode = new Node;
+    enterReview(newNode);
+    if (headNode == nullptr) {
+        headNode = newNode;
+    }
+    else {
+        newNode->next = headNode;
+        headNode = newNode;
+    }
+}
+
+// insert node at tail
+void insertAtTail(Node *&headNode) {
+    Node *newNode = new Node;
+    newNode->next = nullptr;
+    Node *current = headNode;
+    enterReview(newNode);
+    if (headNode == nullptr) {
+        headNode = newNode;
+    }
+    else {
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
